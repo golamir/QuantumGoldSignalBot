25a3ccdaaaed14044f96c595bc906782a20eeb43
golamir <71790026+golamir@users.noreply.github.com>
2026-08-05 08:56:31 +0330

    Create market_fusion.py

---
+++ market_fusion.py
@@ -0,0 +1,74 @@
+from news_engine import get_news_score
+from sentiment_engine import get_sentiment_score
+from analyst_engine import get_analyst_score
+
+
+def get_market_fusion_score(technical_score):
+
+    total_score = technical_score
+
+    reasons = []
+
+
+    news = get_news_score()
+
+    sentiment = get_sentiment_score()
+
+    analyst = get_analyst_score()
+
+
+
+    total_score += news["score"]
+
+    total_score += sentiment["score"]
+
+    total_score += analyst["score"]
+
+
+
+    reasons.extend(news["reasons"])
+
+    reasons.extend(sentiment["reasons"])
+
+    reasons.extend(analyst["reasons"])
+
+
+
+    # محدود کردن امتیاز بین 0 تا 100
+
+    if total_score > 100:
+
+        total_score = 100
+
+
+    if total_score < 0:
+
+        total_score = 0
+
+
+
+    if total_score >= 85:
+
+        decision = "✅ Very strong setup"
+
+
+    elif total_score >= 70:
+
+        decision = "⚠️ Strong setup"
+
+
+    else:
+
+        decision = "❌ Weak setup"
+
+
+
+    return {
+
+        "score": total_score,
+
+        "decision": decision,
+
+        "reasons": reasons
+
+    }
