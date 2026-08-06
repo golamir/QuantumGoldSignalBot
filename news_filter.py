7a0528d8e25e671c9d16abe8bd2d7b3f34cc1b45
golamir <71790026+golamir@users.noreply.github.com>
2026-08-06 11:36:32 +0330

    Update news_filter.py

--- news_filter.py
+++ news_filter.py
@@ -4,22 +4,20 @@ from datetime import datetime
 def check_news():
     """
     Economic news filter
-    Later connected to real economic calendar API
     """
 
     hour = datetime.utcnow().hour
 
-    # ساعات پرریسک تقریبی بازار آمریکا
     high_risk_hours = [12, 13, 14, 15, 16]
 
     if hour in high_risk_hours:
+
         return {
-    "risk": "HIGH",
-    "message": "⚠️ High volatility risk - avoid aggressive entries"
-}
+            "risk": "HIGH",
+            "message": "⚠️ High volatility risk - avoid aggressive entries"
+        }
 
     return {
-        return {
-    "risk": "LOW",
-    "message": "✅ Safe trading period"
-}
+        "risk": "LOW",
+        "message": "✅ Safe trading period"
+    }
